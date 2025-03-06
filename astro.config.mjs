00f3ca9df4ef4a2d47eee21ee52fd9cff24fe7b9
Martin Rojas <317579+martinrojas@users.noreply.github.com>
2025-03-06 01:36:10 -0500

    Add astro-icon integration and update Tailwind CSS dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-
+import icon from "astro-icon";
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +10,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), icon()],
 });
